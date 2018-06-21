7383e473b5cfb8a90082e3b76eb1cbc57c6078d6
swmoon <swmoon@bluedigm.com>
2018-06-21 16:54:40 +0900

    불필요 주석 제거

--- src/main/java/org/openpaas/paasta/portal/api/ApiApplication.java
+++ src/main/java/org/openpaas/paasta/portal/api/ApiApplication.java
@@ -2,34 +2,19 @@ package org.openpaas.paasta.portal.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.ComponentScan;
 
-/**
- * All you need to run a Eureka registration server.
- * 
- * @author Paul Chapman
- */
-@SpringBootApplication
+
+@EnableTurbine
+@EnableCircuitBreaker
 @EnableDiscoveryClient
-@ComponentScan(basePackages = {"org.openpaas.paasta.portal.api"})
+@SpringBootApplication
+@ComponentScan(basePackages = {"org.openpaas.paasta"})
 public class ApiApplication {
-
-	/**
-	 * Run the application using Spring Boot and an embedded servlet engine.
-	 *
-	 * @param args Program arguments - ignored.
-	 */
-	public static void main(String[] args) {
-		// Tell server to look for registration.properties or registration.yml
-
-		SpringApplication.run(ApiApplication.class, args);
-	}
-
-
-
-
-
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
