f027b2f445f7839e9c652ac931cedc884b5686de
dojjang <betterpoul@gmail.com>
2018-06-22 17:18:44 +0900

    sout 삭제

--- src/main/java/org/openpaas/paasta/portal/api/ApiApplication.java
+++ src/main/java/org/openpaas/paasta/portal/api/ApiApplication.java
@@ -4,15 +4,43 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+<<<<<<< Updated upstream
+import org.springframework.context.annotation.ComponentScan;
+=======
+>>>>>>> Stashed changes
+
 import org.springframework.context.annotation.ComponentScan;
 
-
+<<<<<<< Updated upstream
 @EnableCircuitBreaker
 @EnableDiscoveryClient
+=======
+/**
+ * All you need to run a Eureka registration server.
+ *
+ * @author Paul Chapman
+ */
+>>>>>>> Stashed changes
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.openpaas.paasta"})
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"org.openpaas.paasta.portal.api"})
 public class ApiApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
-    }
+
+	/**
+	 * Run the application using Spring Boot and an embedded servlet engine.
+	 *
+	 * @param args Program arguments - ignored.
+	 */
+	public static void main(String[] args) {
+		// Tell server to look for registration.properties or registration.yml
+
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+
+
+
+
+
+
 }
