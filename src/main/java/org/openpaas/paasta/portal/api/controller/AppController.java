08587d64e511d2074b0c99dc53e9afa4952d545e
swmoon <swmoon@bluedigm.com>
2018-06-22 17:57:58 +0900

    잘못삭제된 서비스 복구

--- src/main/java/org/openpaas/paasta/portal/api/controller/AppController.java
+++ src/main/java/org/openpaas/paasta/portal/api/controller/AppController.java
@@ -390,4 +390,10 @@ public class AppController extends Common {
     }
 
 
+    //TODO : 추후 재구현
+    @RequestMapping(value = {Constants.V2_URL + "/app/autoscaling"}, method = RequestMethod.GET)
+    public Map autoscaling() throws Exception {
+        return null;
+    }
+
 }
