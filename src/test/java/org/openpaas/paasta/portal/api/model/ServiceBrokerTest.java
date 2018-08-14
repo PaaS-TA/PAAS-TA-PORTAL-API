6f516affd61849fb85daab48fdd7fbeef584f2c1
swmoon <swmoon@bluedigm.com>
2018-08-14 19:18:57 +0900

    테스트 케이스 적용중

--- src/test/java/org/openpaas/paasta/portal/api/model/ServiceBrokerTest.java
+++
@@ -1,7 +0,0 @@
-//package org.openpaas.paasta.portal.api.model;
-//
-//public class ServiceBrokerTest {
-//    //Field guid of type UUID - was not mocked since Mockito doesn't mock a Final class when 'mock-maker-inline' option is not set
-//}
-//
-////Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
