fa7acd37c2167529b3e596450b74dbd2ad4272b3
박철한 <parkcheolhan@bluedigm.com>
2019-04-22 13:43:09 +0900

    + AppContoller_V3

---
+++ src/main/java/org/openpaas/paasta/portal/api/controller/AppController_V3.java
@@ -0,0 +1,9 @@
+package org.openpaas.paasta.portal.api.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppController_V3 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppController_V3.class);
+
+}
