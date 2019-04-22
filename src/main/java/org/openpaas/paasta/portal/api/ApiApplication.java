8823cab42b3cd403ad1bbd2b869db7d992ce85f7
swmoon <swmoon@bluedigm.com>
2019-04-22 10:48:34 +0900

    Gradle 4.10.0 업데이트

--- src/main/java/org/openpaas/paasta/portal/api/ApiApplication.java
+++ src/main/java/org/openpaas/paasta/portal/api/ApiApplication.java
@@ -3,13 +3,11 @@ package org.openpaas.paasta.portal.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 
 //@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.openpaas.paasta.portal.api"})
 public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
