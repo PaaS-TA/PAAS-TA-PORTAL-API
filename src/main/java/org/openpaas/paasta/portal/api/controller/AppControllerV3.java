f76a47843e434c01f8d8ad587ce436b4504d3b8f
sjchoi <sjchoi@bluedigm.com>
2019-04-22 17:21:32 +0900

    App start,stop

--- src/main/java/org/openpaas/paasta/portal/api/controller/AppControllerV3.java
+++ src/main/java/org/openpaas/paasta/portal/api/controller/AppControllerV3.java
@@ -1,11 +1,15 @@
 package org.openpaas.paasta.portal.api.controller;
 
 import org.openpaas.paasta.portal.api.common.Common;
+import org.openpaas.paasta.portal.api.common.Constants;
+import org.openpaas.paasta.portal.api.model.App;
 import org.openpaas.paasta.portal.api.service.AppServiceV3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -16,4 +20,33 @@ public class AppControllerV3 extends Common {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AppControllerV3.class);
 
+    /**
+     * 앱을 실행한다.
+     *
+     * @param app     the app
+     * @return ModelAndView model
+     * @throws Exception the exception
+     */
+    @RequestMapping(value = {Constants.V3_URL + "/apps/{guid}/actions/start"}, method = RequestMethod.POST)
+    public Map startApp(@RequestBody App app, @RequestHeader(AUTHORIZATION_HEADER_KEY) String token) throws Exception {
+        LOGGER.info("startApp Start ");
+        Map resultMap = appService.startApp(app, token);
+        LOGGER.info("startApp End ");
+        return resultMap;
+    }
+
+    /**
+     * 앱을 중지한다.
+     *
+     * @param app     the app
+     * @return ModelAndView model
+     * @throws Exception the exception
+     */
+    @RequestMapping(value = {Constants.V3_URL + "/apps/{guid}/actions/stop"}, method = RequestMethod.POST)
+    public Map stopApp(@RequestBody App app, @RequestHeader(AUTHORIZATION_HEADER_KEY) String token) throws Exception {
+        LOGGER.info("stopApp Start ");
+        Map resultMap = appService.stopApp(app, token);
+        LOGGER.info("stopApp End ");
+        return resultMap;
+    }
 }
--- src/main/java/org/openpaas/paasta/portal/api/service/AppServiceV3.java
+++ src/main/java/org/openpaas/paasta/portal/api/service/AppServiceV3.java
@@ -1,11 +1,70 @@
 package org.openpaas.paasta.portal.api.service;
 
+import org.apache.wink.common.model.app.AppService;
+import org.cloudfoundry.reactor.client.ReactorCloudFoundryClient;
+import org.openpaas.paasta.portal.api.common.Common;
+import org.openpaas.paasta.portal.api.model.App;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 @Service
-public class AppServiceV3 {
+public class AppServiceV3 extends Common{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AppService.class);
+
+    /**
+     * 앱을 실행한다.
+     *
+     * @param app   the app
+     * @param token the client
+     * @throws Exception the exception
+     */
+    //@HystrixCommand(commandKey = "startApp")
+    public Map startApp(App app, String token) {
+        Map resultMap = new HashMap();
+
+        try {
+            ReactorCloudFoundryClient cloudFoundryClient = cloudFoundryClient(tokenProvider(token));
+            cloudFoundryClient.applicationsV3().start(org.cloudfoundry.client.v3.applications.StartApplicationRequest.builder().applicationId(app.getGuid().toString()).build()).block();
+            resultMap.put("result", true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resultMap.put("result", false);
+            resultMap.put("msg", e.getMessage());
+        }
+
+        return resultMap;
+    }
+
+    /**
+     * 앱을 중지한다.
+     *
+     * @param app     the app
+     * @return ModelAndView model
+     * @throws Exception the exception
+     */
+    //@HystrixCommand(commandKey = "stopApp")
+    public Map stopApp(App app, String token) {
+        Map resultMap = new HashMap();
+
+        try {
+            ReactorCloudFoundryClient cloudFoundryClient = cloudFoundryClient(tokenProvider(token));
+            cloudFoundryClient.applicationsV3().stop(org.cloudfoundry.client.v3.applications.StopApplicationRequest.builder().applicationId(app.getGuid().toString()).build()).block();
+
+            resultMap.put("result", true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resultMap.put("result", false);
+            resultMap.put("msg", e.getMessage());
+        }
+
+        return resultMap;
+    }
+
+
 }
