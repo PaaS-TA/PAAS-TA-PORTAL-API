88b9f1797f7c4c7d2f895ccf9f221a978c419a2e
swmoon <swmoon@bluedigm.com>
2019-04-23 17:05:33 +0900

    app 오류 수정

--- src/main/java/org/openpaas/paasta/portal/api/service/AppServiceV3.java
+++ src/main/java/org/openpaas/paasta/portal/api/service/AppServiceV3.java
@@ -20,15 +20,14 @@ public class AppServiceV3 extends Common {
     private static final Logger LOGGER = LoggerFactory.getLogger(AppServiceV3.class);
 
 
-
     /**
      * 앱을 실행한다.
      *
      * @param token the client
      * @throws Exception the exception
-     * 권한:사용자 권한
+     *                   권한:사용자 권한
      */
-    public void startApp(String appGuid, String token) {
+    public Map startApp(String appGuid, String token) {
         Map resultMap = new HashMap();
 
         try {
@@ -40,18 +39,19 @@ public class AppServiceV3 extends Common {
             resultMap.put("result", false);
             resultMap.put("msg", e.getMessage());
         }
+        return resultMap;
 
     }
 
     /**
      * 앱을 중지한다.
      *
-     * @param token     the client
+     * @param token the client
      * @return ModelAndView model
      * @throws Exception the exception
-     * 권한:사용자 권한
+     *                   권한:사용자 권한
      */
-    public void stopApp(String appGuid, String token) {
+    public Map stopApp(String appGuid, String token) {
         Map resultMap = new HashMap();
 
         try {
@@ -64,35 +64,31 @@ public class AppServiceV3 extends Common {
             resultMap.put("msg", e.getMessage());
         }
 
+        return resultMap;
+
     }
 
     /**
-     *  App env 변수 업데이트
+     * App env 변수 업데이트
      *
-     * @param app the app
+     * @param app   the app
      * @param token the token
      * @return UpdateApplicationEnvironmentVariablesResponse
-     *
+     * <p>
      * 권한 : 사용자 권한
-     *
      */
     public UpdateApplicationEnvironmentVariablesResponse setAppEnv(App app, String token) {
         LOGGER.info("변경사항 있는 환경변수들 ::: " + app.getEnvironment().toString());
 
         ReactorCloudFoundryClient reactorCloudFoundryClient = cloudFoundryClient(tokenProvider(token));
-        UpdateApplicationEnvironmentVariablesResponse updatedAppEnvVar = reactorCloudFoundryClient.applicationsV3()
-                .updateEnvironmentVariables(UpdateApplicationEnvironmentVariablesRequest.builder()
-                        .applicationId(app.getGuid().toString())
-                        .putAllVars(app.getEnvironment())
-                        .build()).block();
+        UpdateApplicationEnvironmentVariablesResponse updatedAppEnvVar = reactorCloudFoundryClient.applicationsV3().updateEnvironmentVariables(UpdateApplicationEnvironmentVariablesRequest.builder().applicationId(app.getGuid().toString()).putAllVars(app.getEnvironment()).build()).block();
 
         LOGGER.info("변경사항 있는 환경변수들은요~~~ ::: " + updatedAppEnvVar.toString());
         return updatedAppEnvVar;
     }
 
 
-
-    public void getSummary(String token,String guid){
+    public void getSummary(String token, String guid) {
 
         ReactorCloudFoundryClient reactorCloudFoundryClient = cloudFoundryClient(tokenProvider(token));
         GetApplicationResponse getApplicationResponse = reactorCloudFoundryClient.applicationsV3().get(org.cloudfoundry.client.v3.applications.GetApplicationRequest.builder().applicationId(guid).build()).block();
@@ -101,27 +97,19 @@ public class AppServiceV3 extends Common {
         GetApplicationProcessStatisticsResponse processStatisticsResponse = reactorCloudFoundryClient.applicationsV3().getProcessStatistics(GetApplicationProcessStatisticsRequest.builder().applicationId(guid).build()).block();
 
 
-
     }
 
     public AppV3 getAppSummary(String guid, String token) {
         ReactorCloudFoundryClient reactorCloudFoundryClient = cloudFoundryClient(tokenProvider(token));
         SummaryApplicationResponse summaryApplicationResponse = reactorCloudFoundryClient.applicationsV2().summary(SummaryApplicationRequest.builder().applicationId(guid).build()).block();
-        GetApplicationResponse getApplicationResponse= reactorCloudFoundryClient.applicationsV3().get(org.cloudfoundry.client.v3.applications.GetApplicationRequest.builder().applicationId(guid).build()).block();
+        GetApplicationResponse getApplicationResponse = reactorCloudFoundryClient.applicationsV3().get(org.cloudfoundry.client.v3.applications.GetApplicationRequest.builder().applicationId(guid).build()).block();
         GetApplicationCurrentDropletResponse getApplicationCurrentDropletResponse = reactorCloudFoundryClient.applicationsV3().getCurrentDroplet(GetApplicationCurrentDropletRequest.builder().applicationId(guid).build()).block();
         GetApplicationProcessResponse getApplicationProcessResponse = reactorCloudFoundryClient.applicationsV3().getProcess(GetApplicationProcessRequest.builder().type("web").applicationId(guid).build()).block();
-        GetApplicationProcessStatisticsResponse processStatisticsResponse =  reactorCloudFoundryClient.applicationsV3().getProcessStatistics(GetApplicationProcessStatisticsRequest.builder().type("web").applicationId(guid).build()).block();
+        GetApplicationProcessStatisticsResponse processStatisticsResponse = reactorCloudFoundryClient.applicationsV3().getProcessStatistics(GetApplicationProcessStatisticsRequest.builder().type("web").applicationId(guid).build()).block();
         GetApplicationEnvironmentResponse getApplicationEnvironmentResponse = reactorCloudFoundryClient.applicationsV3().getEnvironment(GetApplicationEnvironmentRequest.builder().applicationId(guid).build()).block();
-        AppV3 app = AppV3.builder()
-                .applicationResponse(getApplicationResponse)
-                .applicationEnvironmentResponse(getApplicationEnvironmentResponse)
-                .applicationProcessResponse(getApplicationProcessResponse)
-                .applicationCurrentDropletResponse(getApplicationCurrentDropletResponse)
-                .applicationProcessStatisticsResponse(processStatisticsResponse)
-                .summaryApplicationResponse(summaryApplicationResponse).build();
+        AppV3 app = AppV3.builder().applicationResponse(getApplicationResponse).applicationEnvironmentResponse(getApplicationEnvironmentResponse).applicationProcessResponse(getApplicationProcessResponse).applicationCurrentDropletResponse(getApplicationCurrentDropletResponse).applicationProcessStatisticsResponse(processStatisticsResponse).summaryApplicationResponse(summaryApplicationResponse).build();
         return app;
     }
-
 
 
 }
